ea8f7a11461830023e37bc6b0b4ed310b24b59d4
Ameryu <woodguiter28@gmail.com>
2021-02-03 14:02:36 +0900

    jairo test

--- gshogi.js
+++ gshogi.js
@@ -1,55 +1,108 @@
+const shogiboard = Vue.component("Shogiboard",{
+    template:`
+    <div id="grid">
+        <div v-for="n in 64" :class="'grid'+n"></div>
+    </div>
+    `
+})
+
+const shogibox = Vue.component("Shogibox",{
+    props: {
+        rx: Number,
+        ry: Number,
+        rz: Number,
+    },
+    template:`
+    <div id="shogibox" :style='"transform: rotate3d("+rx+","+ry+",0,"+rz+"deg);"'>
+        <Shogiboard></Shogiboard>
+        <div class="surface" id="surface2"></div>
+        <div class="surface" id="surface3"></div>
+        <div class="surface" id="surface4"></div>
+        <div class="surface" id="surface5"></div>
+        <div class="surface" id="surface6"></div>
+    </div>
+    `,
+    components:{
+        "Shogiboard": shogiboard
+    }
+})
+
+const shogirend = new Vue({
+    el: '#wrap',
+    data: {
+        rx: 0,
+        ry: 0,
+        rz: 0,
+    },
+    components:{
+        "Shogibox": shogibox
+    }
+})
+
 const box = document.getElementById("grid");
-let x = 0;
-let r = 0;
-let time = 0;
-let altr = 0;
-let engineflag = false;
-let seti;
+let xr = 0;
+let yr = 0;
+let zr = 0;
 
 document.addEventListener('keydown', (event) => {
-    if(!engineflag){
-        engine();
-        clearInterval(rotateZ);
-    }
+    // if(!engineflag){
+    //     engine();
+    //     clearInterval(rotateZ);
+    // }
     if(event.key=="z"){
-        x= x +200;
+        xr++;
+        zr = zr+5;
+        // xr = xr +5;
     }
     if(event.key=="x"){
-        r = r+10;
+        yr++;
+        zr = zr+5;
+        // yr = yr+5;
     }
-    time = 0;
+    // time = 0;
 }, false);
 
-document.addEventListener('keyup', (event) => {
-    if(event.key=="x"){
-        r = r-5;
-    }
-}, false);
+// document.addEventListener('keyup', (event) => {
+//     if(event.key=="x"){
+//         r = r-5;
+//     }
+// }, false);
 
-const engine = function(){
-    seti = setInterval(()=>{
-        engineflag = true;
-        box.style.transform = "translateX("+x+"px) rotateZ("+r+"deg)";
-        x = x-3;
-        if(x < -3000){
-            clearInterval(seti);
-        }
-        if(time >30000){
-            engineflag = false;
-            x = 0;
-            r=0;
-            clearInterval(seti);
-            setTimeout(() => {
-                rotateZ();
-            }, 1000);
-        }
-        time = time +50;
-    },10)
-}
+// const engine = function(){
+//     seti = setInterval(()=>{
+//         engineflag = true;
+//         box.style.transform = "translateX("+x+"px) rotateZ("+r+"deg)";
+//         x = x-3;
+//         if(x < -3000){
+//             clearInterval(seti);
+//         }
+//         if(time >30000){
+//             engineflag = false;
+//             x = 0;
+//             r=0;
+//             clearInterval(seti);
+//             setTimeout(() => {
+//                 rotateZ();
+//             }, 1000);
+//         }
+//         time = time +50;
+//     },10)
+// }
 
-function rotateZ(){
+function rend(){
     setInterval(()=>{
-        box.style.transform = "translateX("+0+"px) rotateZ("+altr+"deg)";
-        altr = altr + 10;
-    },100)
+        shogirend.rx = xr;
+        shogirend.ry = yr;
+        shogirend.rz = zr;
+    },20)
 }
+rend();
+
+setInterval(() => {
+    if(xr>0){
+        xr--;
+    }
+    if(yr>0){
+        yr--;
+    }
+}, 100);
--- mgshogi.js
+++ mgshogi.js
@@ -1,3 +1,65 @@
+const shogiboard = Vue.component("Shogiboard",{
+    template:`
+    <div id="grid">
+        <div v-for="n in 64" :class="'grid'+n"></div>
+    </div>
+    `
+})
+const shogibox = Vue.component("Shogibox",{
+    props: {
+        rx: Number,
+        ry: Number,
+        rz: Number,
+    },
+    template:`
+    <div id="shogibox" :style='"transform: rotate3d("+rx+","+ry+",0,"+rz+"deg);"'>
+        <Shogiboard></Shogiboard>
+        <div class="surface" id="surface2"></div>
+        <div class="surface" id="surface3"></div>
+        <div class="surface" id="surface4"></div>
+        <div class="surface" id="surface5"></div>
+        <div class="surface" id="surface6"></div>
+    </div>
+    `,
+    components:{
+        "Shogiboard": shogiboard
+    }
+})
+const shogirend = new Vue({
+    el: '#wrap',
+    data: {
+        rx: 0,
+        ry: 0,
+        rz: 0,
+    },
+    components:{
+        "Shogibox": shogibox
+    }
+})
+
+// const box = document.getElementById("grid");
+// let xr = 0;
+// let yr = 0;
+// let zr = 0;
+
+// document.addEventListener('keydown', (event) => {
+//     if(event.key=="z"){
+//         xr++;
+//         zr = zr+5;
+//         // xr = xr +5;
+//     }
+//     if(event.key=="x"){
+//         yr++;
+//         zr = zr+5;
+//         // yr = yr+5;
+//     }
+//     // time = 0;
+// }, false);
+
+
+
+
+
 //jairogameer>>>
 let alpha = 0, beta = 0, gamma = 0;
  
@@ -7,14 +69,11 @@ window.addEventListener("deviceorientation", (e) => {
     beta  = e.beta;   // x軸（左右）まわりの回転の角度（引き起こすとプラス）
     gamma = e.gamma;  // y軸（上下）まわりの回転の角度（右に傾けるとプラス）
 });
- 
 function displayData() {
-    let txt = document.getElementById("root");   // データを表示するdiv要素の取得
-    txt.innerHTML = "alpha: " + alpha + "<br>"  // x軸の値
-                  + "beta:  " + beta  + "<br>"  // y軸の値
-                  + "gamma: " + gamma;          // z軸の値
+    shogirend.rx = alpha;
+    shogirend.ry = beta;
+    shogirend.rz = gamma;
 }
-
-const timer = setInterval(() => {
+const rend = setInterval(()=>{
     displayData();
-}, 100);
+},20)

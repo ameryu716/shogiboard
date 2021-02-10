0123a074165d985c9d2bab984f1347af4807c5a2
ameryu <woodguiter28@gmail.com>
2021-02-10 19:19:15 +0900

    gyrotester2

--- mgshogi.js
+++ mgshogi.js
@@ -89,6 +89,8 @@ window.addEventListener("load",()=>{
             if( response === 'granted' ){
                 //. 許可された場合のみイベントハンドラを追加できる
                 window.addEventListener( "deviceorientation", deviceOrientation );
+                //. 画面上部のボタンを消す
+                document.getElementById("sensorrequest").style.display = "none";
             }
         }).catch( function( e ){
             console.log( e );
@@ -101,12 +103,17 @@ window.addEventListener("load",()=>{
         if( DeviceOrientationEvent.requestPermission && typeof DeviceOrientationEvent.requestPermission === 'function' ){
             //. iOS 13 以上の場合、
             //. 画面上部に「センサーの有効化」ボタンを追加
-            let jairois = confirm("ジャイロセンサーへのアクセスを許可しますか？");
-            if(jairois){
+            const banner = document.createElement("div");
+            banner.style.zIndex = "1";
+            banner.style.position = "absolute"; 
+            banner.style.width = "100%";
+            banner.style.backgroundColor="#000";
+            banner.onclick = ()=>{
                 ClickRequestDeviceSensor();
-            }else{
-                alert("ゲームをプレイできません。");
             }
+            banner.id = "sensorrequest";
+            banner.innerHTML = "<p style='color: rgb(0, 0, 255);'>センサーの有効化</p>";
+            document.body.appendChild(banner);
         }else{
             //. Android または iOS 13 未満の場合、
             //. DeviceOrientationEvent オブジェクトが有効な場合のみ、deviceorientation イベント発生時に deviceOrientaion 関数がハンドリングするよう登録
